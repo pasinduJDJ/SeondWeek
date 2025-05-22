a10edc4a19eb29d6779768be0284c38c4afb7832
pasinduJDJ <pasindudulanjanarj@gmail.com>
2025-05-22 23:59:27 +0530

    Complete Day 05 Task

--- Day04/mysite/home/admin.py
+++ Day04/mysite/home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin (admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', )
+    list_filter = ('created_at',)
+
+    admin.site.register(Post)
+
--- Day04/mysite/home/views.py
+++ Day04/mysite/home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+import markdown
+from datetime import timedelta
+from django.utils import timezone
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    last_week = timezone.now() - timedelta(days=7)
+    posts = Post.objects.filter(created_at__gte=last_week).order_by('-created_at')
+    return render(request, 'home/home.html', {'posts': posts})
 
 def about_view(request):
     return render(request, 'home/about.html')
